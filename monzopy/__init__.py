0650bc967a2986409ba1d3f764d4ddac344d47f6
Jake Martin <jake_martin_@outlook.com>
2023-10-09 00:25:17 +0100

    Added InvalidMonzoAPIResponseError to __init__

--- monzopy/__init__.py
+++ monzopy/__init__.py
@@ -3,4 +3,4 @@ from importlib.metadata import version
 __version__ = version(__name__)
 
 # populate package namespace
-from monzopy.monzopy import AbstractMonzoApi
+from monzopy.monzopy import AbstractMonzoApi, InvalidMonzoAPIResponseError
